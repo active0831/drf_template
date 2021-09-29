1b87b56e1eb08a9c2c375ee3df66b4df1053af52
active0831 <leejaehak87@gmail.com>
2021-09-29 13:45:12 +0900

    back

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -71,6 +71,16 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 
 # Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
 # settings.py
 DATABASES = {
     'default': {
@@ -83,7 +93,7 @@ DATABASES = {
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
